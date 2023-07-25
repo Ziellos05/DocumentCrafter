92610dc12a8102fec2fd5dd09dd60a388ec1f24b
Roland Andres Ortega Ayala <roland.ortega@constructorabolivar.com>
2023-07-25 18:29:05 -0500

    Getting handbooks, creating new handbooks and making the PDF, everything working :)

--- app/components/handbookForm.tsx
+++ app/components/handbookForm.tsx
@@ -1,37 +1,72 @@
 "use client"
 
+import { useRouter } from 'next/navigation';
+
 // Componente que presenta el form a través del cual se puede registrar una nueva URL
 export default function HandbookForm() {
 
+  const router = useRouter();
+
   // Acá se configura la data y se hace el request a la API
-  const handleSubmit = async (event: any) => {
+  const handleSubmit = (event: any) => {
     event.preventDefault();
 
-    const data = {
+    const url = "http://localhost:8080/graphql";
+
+    const CREATE_PDF = `mutation CreateBook($input: HandbookInput!) {
+      createBook(input: $input) {
+        _id
+      }
+    }`;  
+
+    const date = new Date().toLocaleDateString();
+
+    const input = {
       author: event.target.author.value,
       title: event.target.title.value,
-      body: event.target.body.value
+      body: event.target.body.value,
+      date: date
     }
-
-    console.log(data)
+      fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              query: CREATE_PDF,
+              variables: {
+                input: input
+              }
+          })
+      })
+          .then(response => response.json())
+          .then(data => {
+              if (data) {
+                  alert("Document created successfully, check the main page")
+                  router.push('/main');
+              }
+              else {
+                  alert("ERROR :C")
+              }
+          })
 
   }
   return (
 
     <div>
       <form onSubmit={handleSubmit}>
-        <div className="grid gap-6 mb-6 md:grid-cols-2">
+        <div className="grid gap-4 mb-4 md:grid-cols-1">
           <div>
             <label htmlFor="author" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author</label>
-            <input type="text" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"[a-zA-Z0-9]+"} />
+            <input type="text" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
           </div>
           <div>
             <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-            <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"[a-zA-Z0-9]+"} />
+            <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
           </div>
           <div>
             <label htmlFor="body" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</label>
-            <input type="text" id="body" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"[a-zA-Z0-9]+"} />
+            <input type="text" id="body" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
           </div>
         </div>
         <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Generar</button>
--- app/components/handbooksTable.tsx
+++ app/components/handbooksTable.tsx
@@ -2,32 +2,94 @@
 
 import clipboard from '@/public/clipboard-copy-custom.svg';
 import Image from 'next/image';
-import { gql } from "@apollo/client";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
-
-const query = gql`query Handbooks {
-    handbooks {
-      _id
-      date
-    }
-  }`
+import { useEffect, useState } from 'react';
 
 // Componente que muestra en una tabla las URLs generadas para el usuario activo
 export function HandbooksTable() {
 
-    const { data } = useSuspenseQuery(query);
+    const [toDos, setToDos] = useState()
+    const [isLoading, setIsLoading] = useState(false)
 
-    console.log(data)
+    const [pdf, setPdf] = useState()
+    const [isGenerating, setIsGenerating] = useState(false)
+
+    const Test = (id: string) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: CREATE_PDF,
+                variables: {
+                    pdfId: id
+                }
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.data.pdf == "File uploaded successfully!") {
+                    alert("File created successfully, please check drive folder")
+                }
+                else {
+                    alert("ERROR :C")
+                }
+            })
+    }
+
+    const url = "http://localhost:8080/graphql";
+
+    const GET_HANDBOOKS = `query Handbooks {
+    handbooks {
+      _id
+      date
+    }
+  }`;
+
+    const CREATE_PDF = `query Query($pdfId: ID!) {
+    pdf(id: $pdfId)
+  }`;
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: GET_HANDBOOKS,
+            variables: {
+                first: 2
+            }
+        })
+    };
+
+    useEffect(() => {
+        setIsLoading(true)
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data => {
+                setToDos(data) // Set the toDo variable
+                setIsLoading(false)
+            })
+    }, [])
 
     // Acá se configuran los datos y se realiza la petición a la API para obtener las URLs
 
-    type Handbook = {
-        author?: String,
-        title?: String,
-        body?: String
-        _id?: String,
-        date?: String
+    // type Handbook = {
+    //     author?: String,
+    //     title?: String,
+    //     body?: String
+    //     _id?: String,
+    //     date?: String
 
+    // }
+
+    if (isLoading) {
+        return <p>Loading....</p>
+    }
+    if (!toDos) {
+        return <p>No List to show</p>
     }
 
     return (
@@ -37,32 +99,35 @@ export function HandbooksTable() {
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
-                            URL original
+                            Handbook ID
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            URL acortada
+                            Handbook date
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Copiar
+                            Generate PDF
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {data.handbooks.map((handbook: any, key: any) => {
-                        return <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={key}>
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {handbook._id}
-                            </th>
-                            <td className="px-6 py-4">
-                                {handbook.date}
-                            </td>
-                            <td className="px-6 py-4">
-                                <button onClick={() => { navigator.clipboard.writeText(handbookMock.shortened) }} type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-1 py-1 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                    <Image src={clipboard} alt='Copy to clipboard' width={25} height={25} />
-                                </button>
-                            </td>
-                        </tr>
-                    })}
+                    {
+                        // @ts-ignore
+                        toDos.data.handbooks.map((handbook: any, key: any) => {
+                            return <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={key}>
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {handbook._id}
+                                </th>
+                                <td className="px-6 py-4">
+                                    {handbook.date}
+                                </td>
+                                <td className="px-6 py-4">
+                                    <button onClick={() => Test(handbook._id)} type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-1 py-1 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                        <Image src={clipboard} alt='Copy to clipboard' width={25} height={25} />
+                                    </button>
+                                </td>
+                            </tr>
+                        })
+                    }
                 </tbody>
             </table>
         </div>
