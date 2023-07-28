b6878cf1f70d63fcc1e8ef4259483fbe9608a181
Roland Andres Ortega Ayala <roland.ortega@constructorabolivar.com>
2023-07-28 02:51:30 -0500

    Data added to use first demo :)

--- app/components/handbookForm.tsx
+++ app/components/handbookForm.tsx
@@ -54,12 +54,75 @@ export default function HandbookForm() {
             <input type="text" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
           </div>
           <div>
-            <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-            <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
           </div>
           <div>
-            <label htmlFor="body" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</label>
-            <input type="text" id="body" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
+            <label htmlFor="title2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title 2</label>
+            <input type="text" id="title2" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
+          </div>
+          <div>
+            <label htmlFor="edit1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Edit 1</label>
+            <input type="text" id="edit1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
+          </div>
+          <div>
+            <label htmlFor="urlLink1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Link 1</label>
+            <input type="text" id="urlLink1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
+          </div>
+          <div>
+            <label htmlFor="title3" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title 3</label>
+            <input type="text" id="title3" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
+          </div>
+          <div>
+            <label htmlFor="edit2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Edit 2</label>
+            <input type="text" id="edit2" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="ABC12..." required={true} pattern={"/^[\w\-\s]+$/"} />
+          </div>
+          <div className="flex items-start mb-6">
+            <label htmlFor="image2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image 2</label>
+            <select id="image2" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option>Bolivar</option>
+              <option>Evil Bolivar</option>
+            </select>
+          </div>
+          <div className="flex items-start mb-6">
+            <label htmlFor="chosen1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Chosen 1</label>
+            <select id="chosen1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option>Latin</option>
+              <option>English</option>
+            </select>
+          </div>
+          <fieldset>
+            <legend className="sr-only">Align</legend>
+            <div className="flex items-center mb-4">
+              <input id="chosenAlign1" type="radio" name="chosenAlign1" value="left" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" defaultChecked />
+              <label htmlFor="chosenAlign1" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                Left
+              </label>
+            </div>
+            <div className="flex items-center mb-4">
+              <input id="chosenAlign1" type="radio" name="chosenAlign1" value="right" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="chosenAlign1" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                Right
+              </label>
+            </div>
+            <div className="flex items-center mb-4">
+              <input id="chosenAlign1" type="radio" name="chosenAlign1" value="center" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="chosenAlign1" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                Center
+              </label>
+            </div>
+            <div className="flex items-center mb-4">
+              <input id="chosenAlign1" type="radio" name="chosenAlign1" value="justify" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="chosenAlign1" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                Justify
+              </label>
+            </div>
+          </fieldset>
+          <div className="flex items-start mb-6">
+            <div className="flex items-center h-5">
+              <input id="firstPage" type="checkbox" value="YES" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+            </div>
+            <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"><a href="#" className="text-blue-600 hover:underline dark:text-blue-500">Add first page template</a></label>
           </div>
         </div>
         <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Generar</button>
